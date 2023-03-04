98a5cb43716a46c1e4210b1df84776bf0efeb666
jkkk39580 <2733074210@qq.com>
2023-03-04 19:14:38 +0800

    更新: 浏览器通知形式实时歌词接口

---
+++ src/api/lyric/notification.js
@@ -0,0 +1,18 @@
+import { getLyric } from '../../components/Lyric.vue'
+import { storeToRefs } from '../store/pinia'
+
+const { currentMusic } = storeToRefs(playerStore)
+
+function sendNotification() {
+    new Notification("${currentMusic}", {
+        body: '${getLyric}',
+        icon: '../../logo/logo16x16.png'
+    })
+}
+if (window.Notification.permission == "granted") { // 判断是否有权限
+    sendNotification();
+} else if (window.Notification.permission != "denied") {
+    window.Notification.requestPermission(function (permission) { // 没有权限发起请求
+        sendNotification();
+    });
+}
